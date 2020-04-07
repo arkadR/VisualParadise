fd76f7c73f8badcad33ad36d26639633aa3d52e8
arkadR <arkadiusz.rasz@gmail.com>
2020-04-07 16:53:06 +0200

    Added first person player

---
+++ Assets/Player/MouseLook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+  public float mouseSensitivity = 100f;
+
+  public Transform playerBody;
+
+  public float yRotation = 0f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    Cursor.lockState = CursorLockMode.Locked;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    var mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+    var mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+    yRotation -= mouseY;
+    yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+
+    transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
+    playerBody.Rotate(Vector3.up * mouseX);
+  }
+}
---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+  public CharacterController controller;
+  public Transform groundCheck;
+  public LayerMask groundMask;
+
+  public float speed = 12f;
+  public float gravity = -9.81f;
+  public float groundCheckDistance = 0.4f;
+  public float jumpHeight = 2f;
+
+  private Vector2 _velocity;
+  private bool _isGrounded;
+
+  // Update is called once per frame
+  void Update()
+  {
+    ApplyPlayerInput();
+    ApplyGravity();
+    HandleJump();
+  }
+
+  void ApplyPlayerInput()
+  {
+    var x = Input.GetAxis("Horizontal");
+    var z = Input.GetAxis("Vertical");
+
+    var offset = transform.right * x + transform.forward * z;
+
+    controller.Move(offset * speed * Time.deltaTime);
+  }
+
+  void HandleJump()
+  {
+    if (Input.GetButtonDown("Jump") && _isGrounded)
+    {
+      _velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    }
+  }
+
+  void ApplyGravity()
+  {
+    _isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckDistance, groundMask);
+    if (_isGrounded && _velocity.y < 0f)
+      _velocity.y = -2f;
+
+    _velocity.y += gravity * Time.deltaTime;
+    controller.Move(_velocity * Time.deltaTime);
+  }
+}
