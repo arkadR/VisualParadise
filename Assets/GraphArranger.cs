c87978bbe97968a8b4a52863120ffa46c6f7352f
karol <242557@student.pwr.edu.pl>
2020-04-25 00:36:47 +0200

    GraphArranger for rearange graph using repell-attract method

---
+++ Assets/GraphArranger.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphArranger : MonoBehaviour
+{
+    GraphLoader graphLoader;
+    const float equilibriumDistance = 8.0f;
+    const float equilibriumDistance2 = equilibriumDistance * equilibriumDistance;
+
+    private bool arrange = false;
+
+    // Start is called before the first frame update
+    // needs to be called after GraphLoader.Start()
+    void Start()
+    {
+        graphLoader = GameObject.FindObjectOfType<GraphLoader>();
+        Debug.Log("Nodes count: " + graphLoader.physicalNodes.Count);
+        Debug.Log("Edges count: " + graphLoader.physicalEdges.Count);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleArrangeButtonPress();
+    }
+
+    // Update for physics
+    void FixedUpdate()
+    {
+        if (arrange)
+        {
+            StopNodes();
+            Repell();
+            Attract();
+            FixEdges();
+        }
+    }
+
+    void HandleArrangeButtonPress()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            arrange = !arrange;
+            Debug.Log("Presenter " + (arrange ? "on" : "off"));
+            StopNodes();
+        }
+    }
+
+    /// <summary>
+    /// Repell each Node from every other node
+    /// v(d) = equilibrium2 / d
+    /// </summary>
+    void Repell()
+    {
+        foreach (var n in graphLoader.physicalNodes)
+        {
+            Rigidbody nodeRb = n.node.GetComponent<Rigidbody>();
+            foreach (var o in graphLoader.physicalNodes)
+            {
+                if (n == o) continue;
+                Rigidbody otherRb = o.node.GetComponent<Rigidbody>();
+
+                Vector3 direction = nodeRb.position - otherRb.position;
+                float distance = direction.magnitude;
+
+                float velocityMagnitude = equilibriumDistance2 / distance;
+                Vector3 velocity = direction.normalized * velocityMagnitude;
+                otherRb.velocity += -velocity;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Attract two nodes if there is an edge to connect them
+    /// v(d) = d
+    /// </summary>
+    void Attract()
+    {
+        foreach (var e in graphLoader.physicalEdges)
+        {
+            Rigidbody fromRb = e.nodeFrom.node.GetComponent<Rigidbody>();
+            Rigidbody toRb = e.nodeTo.node.GetComponent<Rigidbody>();
+
+            Vector3 direction = fromRb.position - toRb.position;
+            float distance = direction.magnitude;
+
+            Vector3 velocity = direction.normalized * distance;
+
+            fromRb.velocity += -velocity;
+            toRb.velocity += velocity;
+        }
+    }
+
+    /// <summary>
+    /// Update edges position based on corresponding nodes
+    /// </summary>
+    void FixEdges()
+    {
+        foreach(var e in graphLoader.physicalEdges)
+        {
+            var lineRenderer = e.edge.GetComponent<LineRenderer>();
+            var positionFrom = e.nodeFrom.node.GetComponent<Rigidbody>().position;
+            var positionTo = e.nodeTo.node.GetComponent<Rigidbody>().position;
+            lineRenderer.SetPosition(0, positionFrom);
+            lineRenderer.SetPosition(1, positionTo);
+        }
+    }
+
+    /// <summary>
+    /// Set velocity of all nodes to 0
+    /// </summary>
+    void StopNodes()
+    {
+        foreach(var n in graphLoader.physicalNodes)
+        {
+            Rigidbody nodeRb = n.node.GetComponent<Rigidbody>();
+            nodeRb.velocity = new Vector3(0, 0, 0);
+        }
+    }
+}
+
+
--- Assets/GraphLoader.cs
+++ Assets/GraphLoader.cs
@@ -7,63 +7,87 @@ using UnityEngine;
 
 public class GraphLoader : MonoBehaviour
 {
+    public List<PhysicalNode> physicalNodes = new List<PhysicalNode>();
+    public List<PhysicalEdge> physicalEdges = new List<PhysicalEdge>();
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    var graph = LoadGraph("graph1");
-    foreach (var node in graph.nodes)
+    // Start is called before the first frame update
+    void Start()
     {
-      var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-      sphere.transform.position = node.position;
+        var graph = LoadGraph("graph1");
+        foreach (var node in graph.nodes)
+        {
+            var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.position = node.position;
+            AddNodeRigidbody(sphere);
+            physicalNodes.Add(new PhysicalNode { id = node.id, node = sphere });
+        }
+
+        var material = Resources.Load<Material>("Materials/Edge Material");
+        foreach (var edge in graph.edges)
+        {
+            var node1 = graph.nodes.Single(n => n.id == edge.from);
+            var node2 = graph.nodes.Single(n => n.id == edge.to);
+            var line = new GameObject();
+            var lineRenderer = line.AddComponent<LineRenderer>();
+            lineRenderer.SetPosition(0, node1.position);
+            lineRenderer.SetPosition(1, node2.position);
+            lineRenderer.startWidth = 0.2f;
+            lineRenderer.material = material;
+            physicalEdges.Add(new PhysicalEdge { edge = line, nodeFrom = physicalNodes.Single(pn => pn.id == edge.from), nodeTo = physicalNodes.Single(pn => pn.id == edge.to) });
+        }
     }
 
-    var material = Resources.Load<Material>("Materials/Edge Material");
-    foreach (var edge in graph.edges)
+    // Update is called once per frame
+    void Update()
     {
-      var node1 = graph.nodes.Single(n => n.id == edge.from);
-      var node2 = graph.nodes.Single(n => n.id == edge.to);
-      var line = new GameObject();
-      var lineRenderer = line.AddComponent<LineRenderer>();
-      lineRenderer.SetPosition(0, node1.position);
-      lineRenderer.SetPosition(1, node2.position);
-      lineRenderer.startWidth = 0.2f;
-      lineRenderer.material = material;
+
     }
-  }
 
-  // Update is called once per frame
-  void Update()
-  {
+    void AddNodeRigidbody(GameObject node)
+    {
+        Rigidbody sphereRigidBody = node.AddComponent<Rigidbody>();
+        sphereRigidBody.useGravity = false;
+    }
 
-  }
 
+    Graph LoadGraph(string fileName)
+    {
+        var graphJson = Resources.Load(fileName).ToString();
+        return JsonUtility.FromJson<Graph>(graphJson);
+    }
 
-  Graph LoadGraph(string fileName)
-  {
-    var graphJson = Resources.Load(fileName).ToString();
-    return JsonUtility.FromJson<Graph>(graphJson);
-  }
+    [Serializable]
+    class Graph
+    {
+        public List<Node> nodes;
+        public List<Edge> edges;
+    }
 
-  [Serializable]
-  class Graph
-  {
-    public List<Node> nodes;
-    public List<Edge> edges;
-  }
+    [Serializable]
+    class Node
+    {
+        public int id;
+        public Vector3 position;
+    }
 
-  [Serializable]
-  class Node
-  {
-    public int id;
-    public Vector3 position;
-  }
+    [Serializable]
+    class Edge
+    {
+        public int from;
+        public int to;
+        public int weight;
+    }
 
-  [Serializable]
-  class Edge
-  {
-    public int from;
-    public int to;
-    public int weight;
-  }
+    public class PhysicalEdge
+    {
+        public PhysicalNode nodeFrom;
+        public PhysicalNode nodeTo;
+        public GameObject edge;
+    }
+
+    public class PhysicalNode
+    {
+        public int id;
+        public GameObject node;
+    }
 }
