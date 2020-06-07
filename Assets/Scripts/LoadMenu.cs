d92fa362a5f8973428eb72a8772dc561ca6a02a0
arkadR <arkadiusz.rasz@gmail.com>
2020-06-07 13:21:56 +0200

    Initial main menu design

---
+++ Assets/Scripts/LoadMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadMenu : MonoBehaviour
+{
+  public GameObject buttonParent;
+  public GameObject uiButtonPrefab;
+
+  private const int c_buttonGap = 30;
+
+  void Start()
+  {
+    var files = Directory.GetFiles("savedgraphs/", "*.json");
+    for (var i = 0; i < files.Length; i++)
+    {
+      var filePath = files[i];
+      var fileName = Path.GetFileNameWithoutExtension(filePath);
+      var button = Instantiate(uiButtonPrefab, buttonParent.transform);
+      // button.transform.localPosition = new Vector3(0, - (i+1) * c_buttonGap, 0);
+      button.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 400);
+      button.GetComponentInChildren<Text>().text = fileName;
+      button.GetComponent<Button>().onClick.AddListener(() => OnClick(filePath));
+    }
+  }
+
+  private void OnClick(string filePath)
+  {
+
+  }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+  public GameObject mainMenu;
+
+  public GameObject loadMenu;
+  // Start is called before the first frame update
+  void Start()
+  {
+    LoadMainMenu();
+  }
+
+  public void LoadMainMenu()
+  {
+    mainMenu.SetActive(true);
+    loadMenu.SetActive(false);
+  }
+
+  public void LoadButton_OnClick()
+  {
+    mainMenu.SetActive(false);
+    loadMenu.SetActive(true);
+  }
+
+  public void QuitButton_OnClick()
+  {
+    Application.Quit();
+  }
+}
