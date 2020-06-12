c73029638e10dd9ffd6dc602abc46fd506ba80a3
arkadR <arkadiusz.rasz@gmail.com>
2020-06-12 20:31:04 +0200

    Merged save menus

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using Assets.Scripts.Common;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts
 {
@@ -8,30 +9,77 @@ namespace Assets.Scripts
   {
     GraphService graphService;
     public GameObject pauseMenu;
-    public GameObject saveBeforeQuitMenu;
+    public GameObject saveDialog;
+
+    bool _isSaved = false;
 
     public void Start()
     {
       pauseMenu.SetActive(false);
-      saveBeforeQuitMenu.SetActive(false);
+      saveDialog.SetActive(false);
       graphService = FindObjectOfType<GraphService>();
     }
 
     public void SaveButton_OnClick()
     {
-      var filePath = PlayerPrefs.GetString(Constants.GraphFilePathKey);
-      var graph = graphService.Graph;
-      var json = JsonUtility.ToJson(graph);
-      File.WriteAllText(filePath, json);
-      Toast.Instance.Show("Graph saved succesfully!", 4f, Toast.ToastColor.Green);
+      Save();
     }
 
     public void ResumeButton_OnClick() => GameService.Instance.GlobalUnPauseGame();
 
     public void QuitButton_OnClick()
     {
+      if (_isSaved)
+        Quit();
+      else
+      {
+        pauseMenu.SetActive(false);
+        saveDialog.SetActive(true);
+      }
+    }
+
+    public void SaveDialogYes_OnClick()
+    {
+      Save();
+      Quit();
+    }
+
+    public void SaveDialogNo_OnClick()
+    {
+      Quit();
+    }
+
+    public void SaveDialogCancel_OnClick()
+    {
+      pauseMenu.SetActive(true);
+      saveDialog.SetActive(false);
+    }
+
+    public void ShowMenu()
+    {
+      _isSaved = false;
+      pauseMenu.SetActive(true);
+    }
+
+    public void HideMenu()
+    {
       pauseMenu.SetActive(false);
-      saveBeforeQuitMenu.SetActive(true);
+      saveDialog.SetActive(false);
+    }
+
+    void Quit()
+    {
+      SceneManager.LoadScene(Constants.MainMenuScene);
+    }
+
+    void Save()
+    {
+      var filePath = PlayerPrefs.GetString(Constants.GraphFilePathKey);
+      var graph = graphService.Graph;
+      var json = JsonUtility.ToJson(graph);
+      File.WriteAllText(filePath, json);
+      Toast.Instance.Show("Graph saved successfully!", 4f, Toast.ToastColor.Green);
+      _isSaved = true;
     }
   }
 }
