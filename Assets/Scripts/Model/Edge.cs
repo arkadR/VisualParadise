b70ed9fe05f194d951c0ab29d78178821c7d0aef
Michał Engel <michallengel@gmail.com>
2020-06-13 13:32:25 +0200

    Fixed edge label positioning

--- Assets/Scripts/Model/Edge.cs
+++ Assets/Scripts/Model/Edge.cs
@@ -8,40 +8,63 @@ namespace Assets.Scripts.Model
   [Serializable]
   public class Edge : IEquatable<Edge>
   {
+    public int from;
+
+    [NonSerialized] public GameObject gameObject;
     public int id;
     public string label;
-    public int from;
+    [NonSerialized] public Node nodeFrom;
+    [NonSerialized] public Node nodeTo;
     public int to;
     public int weight;
 
-    [NonSerialized] public GameObject gameObject;
-    [NonSerialized] public Node nodeFrom;
-    [NonSerialized] public Node nodeTo;
-
     public Text Text => gameObject.GetComponentInChildren<Text>();
 
-    public string DefaultLabel
-    {
-      get => $"{from}-{to}";
-    }
-
-    public void UpdateTextPosition()
-    {
-      var (x1, y1, z1) = Camera.main.WorldToScreenPoint(nodeFrom.Position);
-      var (x2, y2, z2) = Camera.main.WorldToScreenPoint(nodeTo.Position);
-      var tan = (y1 - y2) / (x1 - x2);
-      tan = float.IsNaN(tan) ? 0 : tan;
-      var angle = Mathf.Atan(tan) * Mathf.Rad2Deg;
-      var centerOfEdgeOnScreen = new Vector3(x1 + x2, y1 + y2, z1 + z2) / 2;
-      Text.SetPositionOnScreen(centerOfEdgeOnScreen);
-      Text.rectTransform.rotation = Quaternion.Euler(0, 0, angle);
-    }
+    public string DefaultLabel => $"{from}-{to}";
 
     public bool Equals(Edge other)
     {
       if (ReferenceEquals(null, other)) return false;
       if (ReferenceEquals(this, other)) return true;
       return id == other.id;
+    }
+
+    public void UpdateTextPosition()
+    {
+      Text.SetPositionOnScreen(LabelPosition());
+      Text.rectTransform.rotation = LabelAngle();
+    }
+
+    Vector3 LabelPosition()
+    {
+      var lineRenderer = gameObject.GetComponent<LineRenderer>();
+      return
+        lineRenderer.positionCount == 2
+          ? StraightLineMiddle()
+          : CurvedLineMiddlePoint(lineRenderer);
+    }
+
+    Vector3 CurvedLineMiddlePoint(LineRenderer lineRenderer)
+    {
+      var middleIndex = lineRenderer.positionCount / 2;
+      var middlePointPosition = lineRenderer.GetPosition(middleIndex);
+      return Camera.main.WorldToScreenPoint(middlePointPosition);
+    }
+
+    Vector3 StraightLineMiddle()
+    {
+      var middle = (nodeFrom.Position + nodeTo.Position) / 2;
+      return Camera.main.WorldToScreenPoint(middle);
+    }
+
+    Quaternion LabelAngle()
+    {
+      var (x1, y1, z1) = Camera.main.WorldToScreenPoint(nodeFrom.Position);
+      var (x2, y2, z2) = Camera.main.WorldToScreenPoint(nodeTo.Position);
+      var tan = (y1 - y2) / (x1 - x2);
+      tan = float.IsNaN(tan) ? 0 : tan;
+      var angle = Mathf.Atan(tan) * Mathf.Rad2Deg;
+      return Quaternion.Euler(0, 0, angle);
     }
 
     public override bool Equals(object obj)
