e5fcb117dad1e879fbb6120c891efbec767a95bb
karol7531 <karol7531lorak@gmail.com>
2020-06-16 04:40:37 +0200

    added ghost node

--- Assets/Scripts/NodeGameObjectFactory.cs
+++ Assets/Scripts/NodeGameObjectFactory.cs
@@ -5,8 +5,12 @@ namespace Assets.Scripts
   public class NodeGameObjectFactory : MonoBehaviour
   {
     public GameObject nodePrefab;
+    public GameObject ghostNodePrefab;
 
     public GameObject CreateNodeGameObject(Vector3 position, Quaternion rotation) =>
       Instantiate(nodePrefab, position, rotation);
+
+    public GameObject CreateGhostNodeGameObject(Vector3 position, Quaternion rotation) =>
+      Instantiate(ghostNodePrefab, position, rotation);
   }
 }
